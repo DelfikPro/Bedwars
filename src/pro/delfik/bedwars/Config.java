5055c1dcb8bd23ede4c5e3436c3b685d558fba4a
DelfikPro <33707322+DelfikPro@users.noreply.github.com>
2018-07-19 21:28:03 +0300

    Добавлены методы, связанные с локацией Лобби

--- src/pro/delfik/bedwars/Config.java
+++ src/pro/delfik/bedwars/Config.java
@@ -1,18 +1,24 @@
 package pro.delfik.bedwars;
 
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.World;
 import org.bukkit.configuration.file.YamlConfiguration;
 
 import java.io.File;
 import java.io.IOException;
 
 public final class Config {
+	private static Location lobbyLocation;
+	private static World lobby;
+	
 	private Config() {}
 	
 	private static YamlConfiguration yml;
 	private static File file;
 	
 	public static void init(String filename) {
-		File file = new File("plugins/" + filename);
+		File file = new File(filename);
 		if (!file.exists()) try {
 			file.createNewFile();
 		} catch (IOException ex) {
@@ -20,6 +26,16 @@ public final class Config {
 		}
 		Config.file = file;
 		yml = YamlConfiguration.loadConfiguration(file);
+		initLobby();
+	}
+	
+	private static void initLobby() {
+		String lobbyname = yml.getString("lobbyworld");
+		if (lobbyname != null) {
+			lobby = Bukkit.getWorld(lobbyname);
+			if (lobby == null) lobby = Bukkit.getWorlds().get(0);
+		} else lobby = Bukkit.getWorlds().get(0);
+		lobbyLocation = lobby.getSpawnLocation().add(0.5, 0.5, 0.5);
 	}
 	
 	public static YamlConfiguration getYml() {
@@ -32,4 +48,7 @@ public final class Config {
 		} catch (IOException e) {throw new RuntimeException(e);}
 	}
 	
+	public static Location getLobbyLocation() {
+		return lobbyLocation;
+	}
 }
