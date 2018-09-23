ce2221fe8b7b18807c89c091f402d94a25feb8af
DelfikPro <33707322+DelfikPro@users.noreply.github.com>
2018-09-23 14:23:45 +0300

    [Add] Статичные импорты, Еда, Инструменты, Сундуки.

--- src/pro/delfik/bedwars/purchase/Purchase.java
+++ src/pro/delfik/bedwars/purchase/Purchase.java
@@ -23,8 +23,13 @@ import java.util.Map;
 
 import static implario.util.Converter.asList;
 import static java.lang.Integer.min;
+import static pro.delfik.bedwars.game.Resource.BRONZE;
+import static pro.delfik.bedwars.game.Resource.GOLD;
+import static pro.delfik.bedwars.game.Resource.IRON;
 import static pro.delfik.lmao.outward.item.Ench.ARROW_FIRE;
 import static pro.delfik.lmao.outward.item.Ench.ARROW_KNOCKBACK;
+import static pro.delfik.lmao.outward.item.Ench.EFFIECENCY_1;
+import static pro.delfik.lmao.outward.item.Ench.EFFIECENCY_3;
 import static pro.delfik.lmao.outward.item.Ench.FIRE_ASPECT;
 import static pro.delfik.lmao.outward.item.Ench.INFINITY;
 import static pro.delfik.lmao.outward.item.Ench.KNOCKBACK_1;
@@ -38,10 +43,12 @@ import static pro.delfik.lmao.outward.item.Ench.SHARPNESS_1;
 import static pro.delfik.lmao.outward.item.Ench.SHARPNESS_2;
 import static pro.delfik.lmao.outward.item.Ench.SHARPNESS_3;
 import static pro.delfik.lmao.outward.item.Ench.SHARPNESS_5;
+import static pro.delfik.lmao.outward.item.Ench.SILK_TOUCH;
+import static pro.delfik.lmao.outward.item.ItemBuilder.create;
 
 public class Purchase implements Listener {
 
-	public static final ItemStack BACK_TO_MAIN = ItemBuilder.create(Material.BED, "§e>> §c§lНазад§e <<");
+	public static final ItemStack BACK_TO_MAIN = create(Material.BED, "§e>> §c§lНазад§e <<");
 	private static final GUI gui = new GUI(Bukkit.createInventory(null, 27, "§0§l- TESTING -"), false) {
 		@Override
 		public void click(InventoryClickEvent event) {
@@ -52,86 +59,107 @@ public class Purchase implements Listener {
 
 			// Блоки
 			new Section(new PurchaseGUI("Блоки", 2, new Deal[] {
-					new Deal(new ItemStack(Material.SANDSTONE, 2), Resource.BRONZE, 1),
-					new Deal(new ItemStack(Material.ENDER_STONE), Resource.BRONZE, 7),
-					new Deal(new ItemStack(Material.IRON_BLOCK), Resource.IRON, 3),
-					new Deal(new ItemStack(Material.GLASS), Resource.BRONZE, 4)
-			}), ItemBuilder.create(Material.QUARTZ_BLOCK, "§a§lБлоки")),
+					new Deal(new ItemStack(Material.SANDSTONE, 2, (short) 2), BRONZE, 1),
+					new Deal(new ItemStack(Material.ENDER_STONE), BRONZE, 7),
+					new Deal(new ItemStack(Material.IRON_BLOCK), IRON, 3),
+					new Deal(new ItemStack(Material.GLOWSTONE, 4), BRONZE, 15),
+					new Deal(new ItemStack(Material.GLASS), BRONZE, 4),
+					new Deal(new ItemStack(Material.CHEST), IRON, 1),
+					new Deal(create(Material.ENDER_CHEST, "§dГиперсундук", "§a§oРесурсы, положенные в него",
+							"§a§oМожно забрать из любого другого гиперсундука.", "§f", "§a§oДоступ к этому сундуку есть только у вашей команды."), GOLD, 1)
+			}), create(Material.QUARTZ_BLOCK, "§a§lБлоки")),
 
 			new Section(new PurchaseGUI("Броня", 2, new Deal[] {
 
-					new ArmorDeal(Resource.BRONZE, 6,
+					new ArmorDeal(BRONZE, 6,
 							new ItemBuilder(Material.IRON_HELMET).enchant(PROTECTION_1).withDisplayName("§aРыцарские доспехи").build(),
 							null,
 							new ItemBuilder(Material.IRON_LEGGINGS).enchant(PROTECTION_1).withDisplayName("§aРыцарские доспехи").build(),
 							new ItemBuilder(Material.IRON_BOOTS).enchant(PROTECTION_1).withDisplayName("§aРыцарские доспехи").build()
 					),
 
-					new ArmorDeal(Resource.IRON, 1, null, new ItemBuilder(Material.IRON_CHESTPLATE)
+					new ArmorDeal(IRON, 1, null, new ItemBuilder(Material.IRON_CHESTPLATE)
 						.enchant(PROTECTION_1).withDisplayName("§aНагрудник").withLore("§7<§f§lОбычный§7>").build(), null, null),
 
-					new ArmorDeal(Resource.IRON, 3, null, new ItemBuilder(Material.IRON_CHESTPLATE)
-						.enchant(PROTECTION_1).withDisplayName("§aНагрудник").withLore("§7<§e§lОсобый§7>").build(), null, null),
+					new ArmorDeal(IRON, 3, null, new ItemBuilder(Material.IRON_CHESTPLATE)
+						.enchant(PROTECTION_2).withDisplayName("§aНагрудник").withLore("§7<§e§lОсобый§7>").build(), null, null),
 
-					new ArmorDeal(Resource.IRON, 9,
+					new ArmorDeal(IRON, 9,
 							new ItemBuilder(Material.DIAMOND_HELMET).withDisplayName("§bЭнергeтическая броня").build(),
 							null,
 							new ItemBuilder(Material.DIAMOND_LEGGINGS).withDisplayName("§bЭнергeтическая броня").build(),
 							new ItemBuilder(Material.DIAMOND_BOOTS).withDisplayName("§bЭнергeтическая броня").build()
 					),
 
-					new ArmorDeal(Resource.IRON, 9, null, new ItemBuilder(Material.DIAMOND_CHESTPLATE)
+					new ArmorDeal(IRON, 9, null, new ItemBuilder(Material.DIAMOND_CHESTPLATE)
 						.withDisplayName("§bНагрудник").withLore("§7<§e§lОсобый§7>").build(), null, null),
 
-					new ArmorDeal(Resource.IRON, 20, null, new ItemBuilder(Material.DIAMOND_CHESTPLATE)
+					new ArmorDeal(IRON, 20, null, new ItemBuilder(Material.DIAMOND_CHESTPLATE)
 						.enchant(PROTECTION_1).withDisplayName("§bНагрудник").withLore("§7<§6§lРедкий§7>").build(), null, null),
 
-					new ArmorDeal(Resource.GOLD, 3, null, new ItemBuilder(Material.DIAMOND_CHESTPLATE)
+					new ArmorDeal(GOLD, 3, null, new ItemBuilder(Material.DIAMOND_CHESTPLATE)
 						.enchant(PROTECTION_2).withDisplayName("§bНагрудник").withLore("§7<§d§lЭпический§7>").build(), null, null),
 
-					new ArmorDeal(Resource.GOLD, 8, null, new ItemBuilder(Material.DIAMOND_CHESTPLATE)
+					new ArmorDeal(GOLD, 8, null, new ItemBuilder(Material.DIAMOND_CHESTPLATE)
 						.enchant(PROTECTION_3).withDisplayName("§bНагрудник").withLore("§7<§c§lЛегендарный§7>").build(), null, null),
-			}), ItemBuilder.create(Material.DIAMOND_CHESTPLATE, "§e§lБроня")),
+			}), create(Material.DIAMOND_CHESTPLATE, "§e§lБроня")),
+
+			new Section(new PurchaseGUI("Инструменты", 2, new Deal[] {
+					new Deal(new ItemBuilder(Material.STONE_PICKAXE).withDisplayName("§fКирка шахтёра").withLore("§7<§f§lОбычная§7>").enchant(EFFIECENCY_1).build(), BRONZE, 4),
+					new Deal(new ItemBuilder(Material.IRON_PICKAXE).withDisplayName("§dМолли").withLore("§7<§6§lРедкая§7>").enchant(EFFIECENCY_1).build(), IRON, 1),
+					new Deal(new ItemBuilder(Material.DIAMOND_PICKAXE).withDisplayName("§bКирка-ювелирка").withLore("§7<§d§lЭпическая§7>")
+									 .enchant(EFFIECENCY_1, SILK_TOUCH).build(), GOLD, 1),
+					new Deal(new ItemBuilder(Material.DIAMOND_PICKAXE).withDisplayName("§cКирка.").withLore("§e§oИ этим всё сказано.", "§7<§c§lЛегендарная§7>")
+									 .enchant(EFFIECENCY_3, FIRE_ASPECT, KNOCKBACK_1).build(), GOLD, 3),
+			}), create(Material.IRON_PICKAXE, "§e§lИнструменты")),
 
 			// Оружие
 			new Section(new PurchaseGUI("Оружие", 2, new Deal[] {
-					new Deal(new ItemBuilder(Material.IRON_SWORD).withDisplayName("§fИголка").withLore("§7<§f§lОбычный§7>").enchant(SHARPNESS_1).build(), Resource.BRONZE, 3),
-					new Deal(new ItemBuilder(Material.DIAMOND_SWORD).withDisplayName("§aАлмеч").withLore("§7<§e§lОсобый§7>").enchant(SHARPNESS_1).build(), Resource.IRON, 1),
+					new Deal(new ItemBuilder(Material.IRON_SWORD).withDisplayName("§fИголка").withLore("§7<§f§lОбычный§7>").enchant(SHARPNESS_1).build(), BRONZE, 3),
+					new Deal(new ItemBuilder(Material.DIAMOND_SWORD).withDisplayName("§aАлмеч").withLore("§7<§e§lОсобый§7>").enchant(SHARPNESS_1).build(), IRON, 1),
 					new Deal(new ItemBuilder(Material.DIAMOND_SWORD).withDisplayName("§bЭкскалибур").withLore("§7<§6§lРедкий§7>")
-									.enchant(SHARPNESS_2, KNOCKBACK_1).build(), Resource.IRON, 5),
+									.enchant(SHARPNESS_2, KNOCKBACK_1).build(), IRON, 5),
 					new Deal(new ItemBuilder(Material.DIAMOND_SWORD).withDisplayName("§6Бесполезная хуета").withLore("§a§oДаже делфик полезнее", "§7<§d§lЭпический§7>")
-									.enchant(SHARPNESS_3, KNOCKBACK_1).build(), Resource.GOLD, 6),
+									.enchant(SHARPNESS_3, KNOCKBACK_1).build(), GOLD, 6),
 					new Deal(new ItemBuilder(Material.DIAMOND_SWORD).withDisplayName("§cКровавая Мэри").withLore("§7<§c§lЛегендарный§7>")
-									.enchant(SHARPNESS_5, FIRE_ASPECT, KNOCKBACK_2).build(), Resource.GOLD, 30)
-			}), ItemBuilder.create(Material.IRON_SWORD, "§c§lОружие")),
+									.enchant(SHARPNESS_5, FIRE_ASPECT, KNOCKBACK_2).build(), GOLD, 30)
+			}), create(Material.IRON_SWORD, "§c§lОружие")),
 
 			new Section(new PurchaseGUI("Луки", 2, new Deal[] {
 					new Deal(new ItemBuilder(Material.BOW).withDisplayName("§aСпортивный лук").withLore("§7<§f§lОбычный§7>")
-									 .enchant(INFINITY).build(), Resource.IRON, 7),
+									 .enchant(INFINITY).build(), IRON, 7),
 					new Deal(new ItemBuilder(Material.BOW).withDisplayName("§fБелый арбалет").withLore("§7<§e§lОсобый§7>")
-									 .enchant(INFINITY, POWER_1).build(), Resource.GOLD, 1),
+									 .enchant(INFINITY, POWER_1).build(), GOLD, 1),
 					new Deal(new ItemBuilder(Material.BOW).withDisplayName("§6Чёрный пистолет").withLore("§7<§6§lРедкий§7>")
-									 .enchant(INFINITY, POWER_1, ARROW_FIRE).build(), Resource.GOLD, 7),
+									 .enchant(INFINITY, POWER_1, ARROW_FIRE).build(), GOLD, 7),
 					new Deal(new ItemBuilder(Material.BOW).withDisplayName("§cТетива смерти").withLore("§7<§d§lЭпический§7>")
-									 .enchant(INFINITY, POWER_3, ARROW_FIRE, ARROW_KNOCKBACK).build(), Resource.GOLD, 30),
-					new Deal(ItemBuilder.create(Material.ARROW, "§6Бесконечный патрон", "§d§o - 'Невероятной судьбы'"), Resource.GOLD, 1)
-			}), ItemBuilder.create(Material.BOW, "§c§lЛуки")),
+									 .enchant(INFINITY, POWER_3, ARROW_FIRE, ARROW_KNOCKBACK).build(), GOLD, 30),
+					new Deal(create(Material.ARROW, "§6Бесконечный патрон", "§d§o - 'Невероятной судьбы'"), GOLD, 1)
+			}), create(Material.BOW, "§c§lЛуки")),
+
+			new Section(new PurchaseGUI("Хавчик", 2, new Deal[] {
+					new Deal(create(Material.APPLE, "§cНаливное яблочко", "§a§oУважает чувства вегитарианцев."), BRONZE, 1),
+					new Deal(create(Material.PUMPKIN_PIE, "§cПирожок с тыквой", "§a§oУважает чувства вегитарианцев."), BRONZE, 2),
+					new Deal(create(Material.CAKE, "§cБисквитный торт", "§a§oУважает чувства вегитарианцев."), BRONZE, 16),
+					new Deal(create(Material.GOLDEN_APPLE, "§6Драгоценный фрукт", "§cЗолото 585-ой пробы, сертифицированное §lВАК",
+							"§a§oУважает чувства вегитарианцев."), GOLD, 2),
+			}), create(Material.APPLE, "§d§lХавчик :>")),
 
 			new Section(new PurchaseGUI("Зелья", 2, new Deal[] {
-					new Deal(new PotionBuilder(PotionType.INSTANT_HEAL).withDisplayName("§dАптечка").build(), Resource.IRON, 3),
-					new Deal(new PotionBuilder(PotionType.INSTANT_HEAL).withAmplifier(1).withDisplayName("§dБольшая аптечка").build(), Resource.IRON, 5),
-					new Deal(new PotionBuilder(PotionType.SPEED).withDuration(180 * 20).withDisplayName("§bСахарный настой").build(), Resource.IRON, 4),
-					new Deal(new PotionBuilder(PotionType.STRENGTH).withDuration(180 * 20).withDisplayName("§cОлимпийский допинг").build(), Resource.GOLD, 4),
+					new Deal(new PotionBuilder(PotionType.INSTANT_HEAL).withDisplayName("§dАптечка").build(), IRON, 3),
+					new Deal(new PotionBuilder(PotionType.INSTANT_HEAL).withAmplifier(1).withDisplayName("§dБольшая аптечка").build(), IRON, 5),
+					new Deal(new PotionBuilder(PotionType.SPEED).withDuration(180 * 20).withDisplayName("§bСахарный настой").build(), IRON, 4),
+					new Deal(new PotionBuilder(PotionType.STRENGTH).withDuration(180 * 20).withDisplayName("§cОлимпийский допинг").build(), GOLD, 4),
 					new Deal(new PotionBuilder(PotionType.INVISIBILITY).withDuration(15 * 20).withDisplayName("§7Ведьмин отвар")
-									 .withLore("§a§oПревращает тебя в хамелеона!").build(), Resource.IRON, 50),
+									 .withLore("§a§oПревращает тебя в хамелеона!").build(), IRON, 50),
 			}), new PotionBuilder(PotionType.STRENGTH).withDisplayName("§d§lЗелья").build()),
 
 			new Section(new PurchaseGUI("Обмен ресурсов", 2, new Deal[] {
-					new Deal(ItemBuilder.setAmount(Resource.BRONZE.getItem(), 32), Resource.IRON, 1),
-					new Deal(ItemBuilder.setAmount(Resource.IRON.getItem(), 1), Resource.BRONZE, 48),
-					new Deal(ItemBuilder.setAmount(Resource.GOLD.getItem(), 1), Resource.IRON, 14),
-					new Deal(ItemBuilder.setAmount(Resource.IRON.getItem(), 7), Resource.GOLD, 1)
-			}), ItemBuilder.create(Material.GOLD_INGOT, "§eОбмен ресурсов"))
+					new Deal(ItemBuilder.setAmount(BRONZE.getItem(), 32), IRON, 1),
+					new Deal(ItemBuilder.setAmount(IRON.getItem(), 1), BRONZE, 48),
+					new Deal(ItemBuilder.setAmount(GOLD.getItem(), 1), IRON, 14),
+					new Deal(ItemBuilder.setAmount(IRON.getItem(), 7), GOLD, 1)
+			}), create(Material.GOLD_INGOT, "§eОбмен ресурсов"))
 	);
 	public static Inventory getInventory() {
 		return gui.inv();
