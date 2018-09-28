1c0ebbb59cc3000655d1c1b49572aa10aeff0c53
DelfikPro <33707322+DelfikPro@users.noreply.github.com>
2018-09-28 18:27:03 +0300

    [Add] Многочисленние обработчики событий
    Взрыв динамита, ломание блока, выпадение предмета

--- src/pro/delfik/bedwars/GameListener.java
+++ src/pro/delfik/bedwars/GameListener.java
@@ -1,15 +1,24 @@
 package pro.delfik.bedwars;
 
+import org.bukkit.GameMode;
 import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.SandstoneType;
+import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.entity.EntityExplodeEvent;
+import org.bukkit.event.entity.ItemSpawnEvent;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.material.Sandstone;
 import pro.delfik.bedwars.game.BWTeam;
+import pro.delfik.bedwars.game.Color;
 import pro.delfik.bedwars.game.Game;
 import pro.delfik.bedwars.game.stuff.GPSTracker;
 import pro.delfik.bedwars.game.stuff.HomeTeleportation;
@@ -17,6 +26,12 @@ import pro.delfik.bedwars.game.stuff.RescuePlatform;
 import pro.delfik.bedwars.purchase.Purchase;
 import pro.delfik.lmao.user.Person;
 import pro.delfik.lmao.util.Vec3i;
+
+import java.util.EnumSet;
+import java.util.LinkedList;
+import java.util.List;
+
+import static org.bukkit.Material.*;
 
 public class GameListener implements Listener {
 	public static boolean dbgclicks = false;
@@ -86,6 +101,65 @@ public class GameListener implements Listener {
 		
 	}
 
+
+	@EventHandler
+	public void onItemSpawn(ItemSpawnEvent e) {
+		switch (e.getEntity().getItemStack().getType()) {
+			case OBSIDIAN:
+			case BED:
+				e.setCancelled(true);
+				// Были ещё какие-то итемы, которым не следует дропаццо, так что лучше пусть будет switch
+		}
+	}
+
+	public static EnumSet<Material> breakable = EnumSet.of(
+			SANDSTONE, ENDER_STONE, IRON_BLOCK, GLASS, CHEST, ENDER_CHEST,
+			GLOWSTONE, CAKE, LADDER, WEB, TRIPWIRE, BED_BLOCK, TNT
+	);
+
+	@EventHandler
+	public void onBlockBreak(BlockBreakEvent e) {
+		Player p = e.getPlayer();
+		if (p.getGameMode() == GameMode.CREATIVE) return;
+		Game game = Game.get(p);
+		if (game == null) {
+			e.setCancelled(true);
+			return;
+		}
+		BWTeam team = game.getTeam(e.getPlayer().getName());
+		if (team == null) {
+			e.setCancelled(true);
+			return;
+		}
+		Material m = e.getBlock().getType();
+		if (!breakable.contains(m)) e.setCancelled(true);
+		switch (e.getBlock().getType()) {
+			case SANDSTONE:
+				Sandstone sandstone = (Sandstone) e.getBlock();
+				if (sandstone.getType() != SandstoneType.SMOOTH) e.setCancelled(true);
+				break;
+			case BED_BLOCK:
+				// ToDo: Обработка поломки кровати
+				Color bed = game.getMap().nearestTeam(e.getBlock().getLocation());
+				if (bed == team.getColor()) {
+					e.setCancelled(true);
+					p.sendMessage("§c" + Messages.YOU_BREAKED_OWN_BED);
+					return;
+				}
+				game.destroyBed(bed, p);
+		}
+	}
+
+	@EventHandler
+	public void onExplode(EntityExplodeEvent e) {
+		List<Block> blocks = new LinkedList<>();
+		for (Block block : e.blockList())
+			if (block.getType() != BED_BLOCK && breakable.contains(block.getType())) blocks.add(block);
+			e.blockList().clear();
+			e.blockList().addAll(blocks);
+	}
+
+	@EventHandler
 	public void onMove(PlayerMoveEvent e) {
 		if (isSameBlock(e.getFrom(), e.getTo())) return;
 		HomeTeleportation.cancelIfTeleporting(e.getPlayer());
