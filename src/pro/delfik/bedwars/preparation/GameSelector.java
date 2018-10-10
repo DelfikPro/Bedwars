373b4b0fe348d25f552843741eb0662bcf06f2d1
DelfikPro <33707322+DelfikPro@users.noreply.github.com>
2018-10-10 23:59:57 +0300

    [Add] Завершение работы над GameSelector'ом

--- src/pro/delfik/bedwars/preparation/GameSelector.java
+++ src/pro/delfik/bedwars/preparation/GameSelector.java
@@ -1,50 +1,58 @@
 package pro.delfik.bedwars.preparation;
 
-import pro.delfik.lmao.outward.item.ItemBuilder;
-import pro.delfik.lmao.outward.gui.GUI;
-import pro.delfik.lmao.outward.gui.GeneralizedGUI;
-import org.bukkit.Bukkit;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import pro.delfik.bedwars.game.Game;
 import pro.delfik.bedwars.game.Game.State;
-import pro.delfik.bedwars.util.FixedArrayList;
-
-import java.util.function.BiConsumer;
+import pro.delfik.lmao.outward.inventory.GUI;
+import pro.delfik.lmao.outward.inventory.SlotGUI;
+import pro.delfik.lmao.outward.item.ItemBuilder;
 
 public class GameSelector {
 
-	private static final BiConsumer<Player, Integer> JOIN = (player, slot) -> {
+
+	private static final GUI ALL_GAMES = new SlotGUI("Список секторов", 2, GameSelector::join);
+
+	private static void join(Player player, int slot, ItemStack itemStack) {
 		if (slot > Game.MAX_RUNNING_GAMES || slot < 0) return;
-		Unit unit = Unit.units.get(slot);
-		switch (unit.getState()) {
-			case NOTHING:
-
+		if (itemStack.getType() == Material.GOLD_BLOCK) {
+			player.closeInventory();
+			player.sendMessage("§eВ разработке.");
+			// ToDo: Spectating running games.
 		}
+	}
 
+	private static final String[] ALPHABET = {
+			"Альфа", "Браво", "Чарли", "Дельта", "Эхо", "Фита", "Гидра",
+			"Олли", "Индо", "Юлия", "Кило", "Лима", "Мега", "Нова",
+			"Оскар", "Пиро", "Квебе", "Ромео", "Сиерра", "Танго", "Юни",
+			"Вижн", "Вольт", "Рентген", "Янки", "Зулу"
 	};
 
+	static {
+		for (int i = 0; i < Game.MAX_RUNNING_GAMES; i++) dummy(i);
+	}
 
-	private static final GUI ALL_GAMES = new GeneralizedGUI(Bukkit.createInventory(null, 18, "Список секторов"), JOIN, null);
+	private static void dummy(int slot) {
+		State state = State.NOTHING;
+		ItemStack item = ItemBuilder.create(state.getMaterial(), "§f§lСектор §e" + ALPHABET[slot],"§f", state.getTitle(), "§cВход закрыт.", "§f");
+		ALL_GAMES.inv().setItem(slot, item);
+	}
 
-	public static class Unit {
-		private static final FixedArrayList<Unit> units = new FixedArrayList<>(Game.MAX_RUNNING_GAMES);
-
-		private final int slot;
-		private final Game game;
-
-		public Unit(int slot) {
-			units.set(slot, this);
-			this.slot = slot;
-			game = Game.get(slot);
+	public static void update(Game game) {
+		State state = game.getState();
+		if (state == State.NOTHING) {
+			dummy(game.getId());
+			return;
 		}
-		public ItemStack generateItem() {
-			State state = getState();
-			return ItemBuilder.create(state.getMaterial(), "§f§lСектор " + (slot + 1), "§f", state.getTitle(),
-					state == State.GAME ? "§f>> §e§lНаблюдать §f<<" : "§cВход закрыт.", "§f");
-		}
-		public State getState() {
-			return game == null ? State.NOTHING : game.getState();
-		}
+		ItemStack item = ItemBuilder.create(state.getMaterial(), "§f§lСектор §e" + ALPHABET[game.getId()],"§f", state.getTitle(),
+				"§f§lКарта: §e" + game.getMap().getName(), state == State.GAME ? "§f>> §e§lНаблюдать §f<<" : "§cВход закрыт.", "§f");
+		ALL_GAMES.inv().setItem(game.getId(), item);
+	}
+
+	public static Inventory getInventory() {
+		return ALL_GAMES.inv();
 	}
 }
