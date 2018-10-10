12a3b9e5f3d4f471f25e091abc414a2a3cc68453
DelfikPro <33707322+DelfikPro@users.noreply.github.com>
2018-10-10 23:51:01 +0300

    [Add] Регистрация новых команд и слушателей

--- src/pro/delfik/bedwars/BWPlugin.java
+++ src/pro/delfik/bedwars/BWPlugin.java
@@ -1,12 +1,15 @@
 package pro.delfik.bedwars;
 
-import org.bukkit.Bukkit;
-import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 import pro.delfik.bedwars.command.CommandBedWars;
+import pro.delfik.bedwars.command.CommandKill;
+import pro.delfik.bedwars.command.CommandSetupSlots;
+import pro.delfik.bedwars.game.Game;
+import pro.delfik.bedwars.game.stuff.ThorBone;
 import pro.delfik.bedwars.purchase.Purchase;
 import pro.delfik.lmao.command.handle.LmaoCommand;
+import pro.delfik.lmao.misc.Garpoon;
 import pro.delfik.lmao.util.Registrar;
 
 /**
@@ -19,10 +22,10 @@ public class BWPlugin extends JavaPlugin {
 	public static BWPlugin instance;
 	
 	private final LmaoCommand[] COMMANDS =
-			{new CommandBedWars()};
+			{new CommandBedWars(), new CommandKill(), new CommandSetupSlots()};
 
 	private final Listener[] LISTENERS =
-			{new Purchase(), new GeneralListener(), new GameListener()};
+			{new Purchase(), new GeneralListener(), new GameListener(), CommandSetupSlots.instance, new ThorBone()};
 	
 	
 	@Override
@@ -32,16 +35,18 @@ public class BWPlugin extends JavaPlugin {
 	
 	@Override
 	public void onEnable() {
+		Garpoon.disable();
 		Registrar registrar = new Registrar(this);
 		for (LmaoCommand cmd : COMMANDS) registrar.regCommand(cmd);
 		for (Listener listener : LISTENERS) registrar.regEvent(listener);
 
-		Bukkit.getPluginCommand("kill").setExecutor((s, c, l, a) -> {
-			((Player) s).setHealth(0);
-			s.sendMessage("§6Вы самоубились.");
-			return true;
-		});
-
 		ConfigReader.initMaps();
+	}
+
+	@Override
+	public void onDisable() {
+		for (Game game : Game.running()) if (game != null) {
+			game.stop();
+		}
 	}
 }
